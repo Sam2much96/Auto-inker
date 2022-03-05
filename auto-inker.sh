6b7be8599c3f8995a1a8cbb79a700b4841e7c47e
Inhumanity Studios <44836046+Sam2much96@users.noreply.github.com>
2022-03-05 21:14:40 +0100

    Update auto-inker.sh

--- auto-inker.sh
+++ auto-inker.sh
@@ -9,6 +9,29 @@ set -x #for debugging
 echo 'Checking for Missing Depencencies'
 echo 'Auto-Inking-Bash-Script v1.1 -Written by Samuel Onome Harrison'
 
+generate_shared_lib_links () {
+	# liblinks - generate symbolic links 
+# given libx.so.0.0.0 this would generate links for libx.so.0.0, libx.so.0, libx.so
+#
+
+# By adding sudo to the ls command, we get permission to do the linking (or get an empty list)
+LIBFILES=`sudo ls lib*.so.*`
+for FILE in $LIBFILES;
+   do
+   echo $FILE
+    shortlib=$FILE
+    basename=$FILE
+    while extn=$(echo $shortlib | sed -n '/\.[0-9][0-9]*$/s/.*\(\.[0-9][0-9]*\)$/\1/p')
+          [ -n "$extn" ]
+    do
+        shortlib=$(basename $shortlib $extn)
+        sudo ln -fs $basename $shortlib
+        basename=$shortlib
+    done
+
+   done
+
+} 
 
 install_potrace (){
 
